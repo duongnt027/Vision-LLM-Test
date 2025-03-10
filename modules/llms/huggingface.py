c436b3789966d85e8555119198867389b061a4d5
cdghhhiilnnotu <nguyenthanhduong22072002@gmail.com>
2025-03-10 09:45:26 +0700

    edit huggingface llm

--- modules/llms/huggingface.py
+++ modules/llms/huggingface.py
@@ -8,19 +8,20 @@ class HuggingFaceLLM(BaseLLM):
     def __init__(self, model_name="meta-llama/Llama-3.2-3B-Instruct", system_prompt = "", hf_token=""):
         super().__init__(model_name, system_prompt)
         self.hf_token = hf_token
-
-    def query(self, prompt, temperature=0.7, max_tokens=128000):
-        pipe = pipeline(
+        self.pipe = pipeline(
             model=self.model_name,
             torch_dtype=torch.bfloat16,
             device_map="auto",
             token=self.hf_token
         )
+
+    def query(self, prompt, temperature=0.7, max_tokens=128000):
+        
         messages = [
             {"role": "system", "content": self.system_prompt},
             {"role": "user", "content": prompt},
         ]
-        outputs = pipe(
+        outputs = self.pipe(
             messages,
             max_new_tokens=max_tokens,
         )
